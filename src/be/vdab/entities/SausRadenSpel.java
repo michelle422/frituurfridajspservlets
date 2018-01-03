3177aaf72b6fc4e4a5cfdf86182584fbdb480689
cheli <cheli@192.168.0.155>
2018-01-03 22:20:11 +0100

    commit met saus raden

---
+++ src/be/vdab/entities/SausRadenSpel.java
@@ -0,0 +1,51 @@
+package be.vdab.entities;
+
+import java.io.Serializable;
+import java.util.stream.IntStream;
+
+public class SausRadenSpel implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private static final int MAX_BEURTEN = 10;
+	private final String saus;
+	private final StringBuilder sausMetPuntjes;
+	private int verkeerdeBeurten;
+	
+	public SausRadenSpel(String saus) {
+		this.saus = saus;
+		sausMetPuntjes = new StringBuilder(saus.length());
+		IntStream.rangeClosed(1, saus.length())
+			.forEach(teller -> sausMetPuntjes.append('.'));
+	}
+
+	public String getSaus() {
+		return saus;
+	}
+
+	public String getSausMetPuntjes() {
+		return sausMetPuntjes.toString();
+	}
+
+	public int getVerkeerdeBeurten() {
+		return verkeerdeBeurten;
+	}
+
+	public void doeGok(char letter) {
+		int letterIndex = saus.indexOf(letter);
+		if (letterIndex == -1) {
+			verkeerdeBeurten++;
+		} else {
+			do {
+				sausMetPuntjes.setCharAt(letterIndex, letter);
+				letterIndex = saus.indexOf(letter, letterIndex+1);
+			} while (letterIndex != -1);
+		}
+	}
+	
+	public boolean isGewonnen() {
+		return sausMetPuntjes.indexOf(".") == -1;
+	}
+	
+	public boolean isVerloren() {
+		return verkeerdeBeurten == MAX_BEURTEN;
+	}
+}
